f98016b713de57840f0fdcebb11032a0f11c963b
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2019-05-03 16:06:43 -0400

    100% TypeScript

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,11 +2,11 @@ import { RefObject } from 'react'
 
 declare module 'react-hook-inview'
 
-type Entry = IntersectionObserverEntry[];
+type Entry = IntersectionObserverEntry;
 type Observer = IntersectionObserver;
 
 export interface Options extends IntersectionObserverInit {
-  target: RefObject<HTMLElement>;
+  target: RefObject<Element>;
   onEnter?: (entry: Entry, observer: Observer) => void;
   onLeave?: (entry: Entry, observer: Observer) => void;
   unobserveOnEnter?: boolean;
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
-// This is experimental and not compiled
-
 import { useEffect } from 'react'
 import { Options } from './index.d'
 
 export const useInView = ({
+  target,
   root = null,
   rootMargin = '0px',
   threshold = 0,
-  target = null,
-  onEnter = (): void => null,
-  onLeave = (): void => null,
+  onEnter = (): void => {},
+  onLeave = (): void => {},
   unobserveOnEnter = false,
 }: Options): void => {
   const callback: IntersectionObserverCallback = ([entry], observer): void => {
     if (entry.isIntersecting) {
-      onEnter([entry], observer)
-      if (unobserveOnEnter) observer.unobserve(target.current)
+      onEnter(entry, observer)
+      if (target.current && unobserveOnEnter) observer.unobserve(target.current)
     } else {
-      onLeave([entry], observer)
+      onLeave(entry, observer)
     }
   }
 
-  useEffect((): () => void => {
+  useEffect(() => {
     const observer = new IntersectionObserver(callback, {
       root,
       rootMargin,
       threshold,
     })
 
-    if (target) observer.observe(target.current)
-    return (): void => observer.unobserve(target.current)
+    if (target.current) observer.observe(target.current)
+    return () => {
+      if (target.current) {
+        return observer.unobserve(target.current)
+      }
+    }
   }, [])
 }
