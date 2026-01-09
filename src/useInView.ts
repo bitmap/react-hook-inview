b6977cb6c93e84407802de9c838953d8d8bd7601
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2026-01-09 14:48:10 -0500

    Code quality audit

--- src/useInView.ts
+++ src/useInView.ts
@@ -1,47 +1,49 @@
-import {
-  useEffect,
-  useState,
-  useCallback,
-} from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import useObserver from "./useObserver";
 
 interface State {
-  inView: boolean
-  entry: IntersectionObserverEntry | null
-  observer: IntersectionObserver | null
+  inView: boolean;
+  entry: IntersectionObserverEntry | null;
+  observer: IntersectionObserver | null;
 }
 
-export interface Options extends IntersectionObserverInit {
-  unobserveOnEnter?: boolean
+export interface UseInViewOptions extends IntersectionObserverInit {
+  unobserveOnEnter?: boolean;
   /**
    * @deprecated Use setRef callback.
    */
-  target?: React.RefObject<Element | null>
+  target?: React.RefObject<Element | null>;
   /**
    * @deprecated Use hook `useInViewEffect` to access observer callback.
    */
-  onEnter?: (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void
+  onEnter?: (
+    entry: IntersectionObserverEntry,
+    observer: IntersectionObserver,
+  ) => void;
   /**
    * @deprecated Use hook `useInViewEffect` to access observer callback.
    */
-  onLeave?: (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void
+  onLeave?: (
+    entry: IntersectionObserverEntry,
+    observer: IntersectionObserver,
+  ) => void;
   /**
    * Default `inView` state
    */
-  defaultInView?: boolean
+  defaultInView?: boolean;
 }
 
 interface UseInView {
   (
-    options?: Options,
-    externalState?: React.ComponentState[]
+    options?: UseInViewOptions,
+    externalState?: React.ComponentState[],
   ): [
     (node: Element | null) => void,
     State["inView"],
     State["entry"],
     State["observer"],
-  ]
+  ];
 }
 
 /**
@@ -50,7 +52,16 @@ interface UseInView {
  * @param externalState React.ComponentState[]
  */
 const useInView: UseInView = (
-  { root, rootMargin, threshold, unobserveOnEnter, target, onEnter, onLeave, defaultInView } = {},
+  {
+    root,
+    rootMargin,
+    threshold,
+    unobserveOnEnter,
+    target,
+    onEnter,
+    onLeave,
+    defaultInView,
+  } = {},
   externalState = [],
 ) => {
   const [state, setState] = useState<State>({
@@ -59,35 +70,38 @@ const useInView: UseInView = (
     observer: null,
   });
 
-  const callback = useCallback<IntersectionObserverCallback>(([entry], observer) => {
-    const inThreshold = observer.thresholds.some((t) => entry.intersectionRatio >= t);
-    const inView = inThreshold && entry.isIntersecting;
+  const callback = useCallback<IntersectionObserverCallback>(
+    ([entry], observer) => {
+      const inThreshold = observer.thresholds.some(
+        (t) => entry.intersectionRatio >= t,
+      );
+      const inView = inThreshold && entry.isIntersecting;
 
-    setState({
-      inView,
-      entry,
-      observer,
-    });
+      setState({
+        inView,
+        entry,
+        observer,
+      });
 
-    // unobserveOnEnter
-    if (inView && unobserveOnEnter) {
-      observer.unobserve(entry.target);
-      observer.disconnect();
-    }
+      // unobserveOnEnter
+      if (inView && unobserveOnEnter) {
+        observer.disconnect();
+      }
 
-    // Legacy callbacks
-    if (inView) {
-      onEnter?.(entry, observer);
-    } else {
-      onLeave?.(entry, observer);
-    }
-  }, [onEnter, onLeave, unobserveOnEnter]);
-
-  const setTarget = useObserver(
-    callback,
-    { root, rootMargin, threshold },
-    [unobserveOnEnter, ...externalState],
+      // Legacy callbacks
+      if (inView) {
+        onEnter?.(entry, observer);
+      } else {
+        onLeave?.(entry, observer);
+      }
+    },
+    [onEnter, onLeave, unobserveOnEnter],
   );
+
+  const setTarget = useObserver(callback, { root, rootMargin, threshold }, [
+    unobserveOnEnter,
+    ...externalState,
+  ]);
 
   // Legacy 'target' option
   useEffect(() => {
